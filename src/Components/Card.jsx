4d51d2671fd26c52db409b35720a9a26f658e531
9insane9 <145199591+9insane9@users.noreply.github.com>
2025-02-18 16:16:50 +0200

    make cards 3d, fit images

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,20 @@
+import Tilt from 'react-parallax-tilt'
+
 export default function Card({ src, alt, playTurnFn }) {
-    
+
     return (
-        <div className="card" onClick={() => playTurnFn(alt)}>
-            <img className="card-img" src={src} alt={alt} />
-            <h1 className="card-title">{alt}</h1>
-        </div>
+        <Tilt
+            glareEnable={true}
+            glareMaxOpacity={0.4}
+            perspective={750}
+            glareBorderRadius='10px'   
+        >
+            <div className="card" onClick={() => playTurnFn(alt)}>
+                <div className='img-wrapper'>
+                    <img className="card-img" src={src} alt={alt} />
+                </div>
+                <h2 className="card-title">{alt}</h2>
+            </div>
+        </Tilt>
     )
 }
